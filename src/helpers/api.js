aed132368f25df60fe24b3c951afb4aa246c9def
Sergey Chepukhin <s.chepukhin@iq-adv.ru>
2022-04-27 20:09:47 +0500

    step-7 | fix

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = (url) => {
   const proxyUrl = new URL('https://allorigins.hexlet.app');
+  proxyUrl.pathname = 'get';
   proxyUrl.searchParams.append('url', url);
   proxyUrl.searchParams.append('disableCache', 'true');
   return axios.get(proxyUrl.toString());
